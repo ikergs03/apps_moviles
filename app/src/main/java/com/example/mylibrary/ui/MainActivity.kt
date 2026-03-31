846bd06bb8c4086da9d95c93b802cf83f837056c
Iker González Sánchez <gonzalez.sanchez.iker.giner@gmail.com>
2026-03-31 12:19:13 +0200

    feat: agrega MainActivity y estructura mínima de la app

---
+++ app/src/main/java/com/example/mylibrary/ui/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.mylibrary.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.mylibrary.R
+import com.example.mylibrary.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        binding.toolbar.post {
+            binding.toolbar.navigationIcon?.setTint(getColor(R.color.gray_toolbar_icon))
+        }
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfig = AppBarConfiguration(setOf(R.id.libraryFragment))
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        return navHostFragment.navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
