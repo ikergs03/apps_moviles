aaf56b6eb5432bcaf23ff75b631bcb238a9524f8
Iker González Sánchez <gonzalez.sanchez.iker.giner@gmail.com>
2026-05-03 20:24:27 +0200

    feat(firebase): configurar App Check con Play Integrity

--- app/src/main/java/com/example/mylibrary/MyLibraryApplication.kt
+++ app/src/main/java/com/example/mylibrary/MyLibraryApplication.kt
@@ -1,7 +1,23 @@
 package com.example.mylibrary
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyLibraryApplication : Application()
+class MyLibraryApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        
+        // Inicializar Firebase
+        FirebaseApp.initializeApp(this)
+        
+        // Inicializar App Check con Play Integrity
+        val appCheck = FirebaseAppCheck.getInstance()
+        appCheck.installAppCheckProviderFactory(
+            PlayIntegrityAppCheckProviderFactory.getInstance()
+        )
+    }
+}
